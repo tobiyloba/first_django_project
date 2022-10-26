83224ef20c1595496b7585b4aa7db481e089b7e0
Tobiloba Ojo <ojotobi6@gmail.com>
2022-10-26 19:24:02 +0100

    Push To Github

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,29 @@
+from statistics import mode
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    age = models.PositiveSmallIntegerField()
+    def __str__(self):
+        return self.first_name
+    
+
+class Song(models.Model):
+    Artiste = models.ForeignKey(Artiste, on_delete = models.CASCADE)
+    title = models.CharField(max_length=200)
+    date_released = models.DateField()
+    likes = models.PositiveSmallIntegerField()
+    artiste_id = models.PositiveSmallIntegerField()
+    def __str__(self):
+        return self.title
+
+class Lyric(models.Model):
+    Song = models.ForeignKey(Song, on_delete = models.CASCADE)
+    content = models.CharField(max_length=2000)
+    song_id = models.PositiveSmallIntegerField()
+    def __str__(self):
+        return self.Song.title
--- musicapp/urls.py
+++ musicapp/urls.py
@@ -19,5 +19,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name = "index")
+    path('index/', views.index, name ="index")
 ]
